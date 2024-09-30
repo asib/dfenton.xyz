b6bb4098576f03cf10bf1376c073d90e823bd19c
Jacob Fenton <jacob+github@dfenton.xyz>
2024-09-30 21:52:27 +0100

    add posthog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+posthog.init('phc_oYpIQ4PC4gP5SyBjBlpbXktnljkbnkSfZs4CjJ6gzaW', {
+  api_host: 'https://eu.i.posthog.com',
+  person_profiles: 'always',
+})
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
