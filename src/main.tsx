671166276c18e0539c481bc11d554f3a789b257e
Sprite <noreply@sprites.dev>
2026-05-28 14:14:17 +0000

    remove posthog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-
-posthog.init('phc_oYpIQ4PC4gP5SyBjBlpbXktnljkbnkSfZs4CjJ6gzaW', {
-  api_host: 'https://eu.i.posthog.com',
-  person_profiles: 'always',
-})
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PostHogProvider client={posthog}>
-      <App />
-    </PostHogProvider>
+    <App />
   </StrictMode>,
 )
